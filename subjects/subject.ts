b5e580f7773d3a27f4eef8face5ac667643096f6
orlaqp <orlaqp@gmail.com>
2019-05-29 18:13:08 -0400

    2 files modified

--- subjects/subject.ts
+++ subjects/subject.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 const s = new Subject();
 
 // then test with subscription before emitting values
-// s.subscribe(x => console.log(x));
+// s.subscribe(console.log);
 
 s.next(1);
 s.next(2);
@@ -11,6 +11,6 @@ s.next(3);
 s.next(4);
 
 // test with subscription after values have been emitted
-s.subscribe(x => console.log(x));
+// s.subscribe(console.log);
 
 console.log('done');
